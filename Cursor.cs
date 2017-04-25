d4adc77f56af02644f674c8512a6a017966b03c3
6420 Laptop\Toby C <prog4life75@gmail.com>
2017-04-24 21:03:45 -0400

    Initial commit

---
+++ Cursor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+	//Variables
+	//CameraRaycaster cameraRaycaster;
+
+
+	void Start () {
+		//cameraRaycaster = GetComponent<CameraRaycaster>();
+	}//End Start
+	
+
+	void Update () {
+		//Debug.Log("Layer hit = " + cameraRaycaster.layerHit);
+	}//End Update
+}
